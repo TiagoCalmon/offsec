cd7d971f52b33ff9f42ad71fcc4351922bab7918
TiagoCalmon <63727674+TiagoCalmon@users.noreply.github.com>
2023-11-28 10:50:29 -0300

    Create enumeration.ps1

---
+++ PEN200/powershell/enumeration.ps1
@@ -0,0 +1,18 @@
+# Store the domain object in the $domainObj variable
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+# Print the variable
+#$domainObj
+
+# Store the PdcRoleOwner name to the $PDC variable
+$PDC = $domainObj.PdcRoleOwner.Name
+# Print the $PDC variable
+#$PDC
+
+$DN = ([adsi]'').distinguishedName
+
+# Print the $DN variable
+#$DN
+
+$LDAP = "LDAP://$PDC/$DN"
+$LDAP
