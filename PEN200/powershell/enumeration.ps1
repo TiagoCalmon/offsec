e8b240ba2dbea33ea136b813bb80909c7a13ab57
TiagoCalmon <63727674+TiagoCalmon@users.noreply.github.com>
2023-11-28 11:28:38 -0300

    Update enumeration.ps1

--- PEN200/powershell/enumeration.ps1
+++ PEN200/powershell/enumeration.ps1
@@ -15,4 +15,21 @@ $DN = ([adsi]'').distinguishedName
 #$DN
 
 $LDAP = "LDAP://$PDC/$DN"
-$LDAP
+#$LDAP
+$direntry = New-Object System.DirectoryServices.DirectoryEntry($LDAP)
+
+$dirsearcher = New-Object System.DirectoryServices.DirectorySearcher($direntry)
+#$dirsearcher.filter="samAccountType=805306368"
+$dirsearcher.filter="name=jeffadmin"
+$result =  $dirsearcher.FindAll()
+
+Foreach($obj in $result)
+{
+    Foreach($prop in $obj.Properties)
+    {
+        #$prop
+	$prop.memberof
+    }
+
+    Write-Host "-------------------------------"
+}
